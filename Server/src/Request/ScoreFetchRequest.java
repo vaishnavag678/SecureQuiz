094dadf481168f90fb004bfa5c40eaeefe82d589
abhi-biswas <mpsabhijeet@gmail.com>
2019-05-15 02:09:58 +0530

    Added ScoreFetchRequest.

---
+++ Server/src/Request/ScoreFetchRequest.java
@@ -0,0 +1,38 @@
+package Request;
+
+import Constants.reqEnum;
+
+import java.io.Serializable;
+
+public class ScoreFetchRequest implements Serializable {
+    int studentId,qId;
+
+    public ScoreFetchRequest(int studentId, int qId) {
+        this.studentId = studentId;
+        this.qId = qId;
+    }
+
+    public ScoreFetchRequest() {
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getqId() {
+        return qId;
+    }
+
+    public void setqId(int qId) {
+        this.qId = qId;
+    }
+    @Override
+    public String toString()
+    {
+        return  "" + reqEnum.RankFetchRequest;
+    }
+}
