45ae39f7ec75159315cc0133b6532e1f5c3c6031
vaishnavag678 <vaishnavagrawal5@gmail.com>
2019-05-15 02:02:26 +0530

    subject request by student

--- Server/src/Request/SubjectAddRequestTeacher.java
+++ Server/src/Request/SubjectAddRequestTeacher.java
@@ -1,7 +1,29 @@
 package Request;
 
+
 import java.io.Serializable ;
+import java.util.*;
+import javafx.util.Pair;
 public class SubjectAddRequestTeacher {
 
+    private String teacherid;
+    ArrayList<Integer>  id=new ArrayList<Integer>(1000);
 
+    public SubjectAddRequestTeacher(String teacherid, ArrayList<Integer> id) {
+        this.teacherid = teacherid;
+        this.id = id;
+    }
+
+    public String getTeacherid() {
+        return teacherid;
+    }
+
+    public void setTeacherid(String teacherid) {
+        this.teacherid = teacherid;
+    }
+       /*  @Override
+    public String toString(){
+
+
+    }*/
 }
