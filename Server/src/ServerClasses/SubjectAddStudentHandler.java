d47cf2dda7d272624f93a7dfef3e8a9b89f7c825
vaishnavag678 <vaishnavagrawal5@gmail.com>
2019-05-18 21:21:40 +0530

    adding studentaddrequest handler

---
+++ Server/src/ServerClasses/SubjectAddStudentHandler.java
@@ -0,0 +1,12 @@
+package ServerClasses;
+
+import java.util.*;
+import java.io.Serializable ;
+import Constants.reqEnum;
+
+
+
+
+public class SubjectAddStudentHandler implements Serializable{
+    
+}
