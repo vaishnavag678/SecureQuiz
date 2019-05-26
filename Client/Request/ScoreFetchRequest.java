90d3433cf9edb2d6f68a93b98057cf74ea729a31
abhi-biswas <mpsabhijeet@gmail.com>
2019-05-26 19:34:51 +0530

    Fixed some bugs.
    Signed-off-by: abhi-biswas <mpsabhijeet@gmail.com>

--- Client/Request/ScoreFetchRequest.java
+++ Client/Request/ScoreFetchRequest.java
@@ -33,6 +33,6 @@ public class ScoreFetchRequest implements Serializable {
     @Override
     public String toString()
     {
-        return  "" + reqEnum.RankFetchRequest;
+        return  "" + reqEnum.ScoreFetchRequest;
     }
 }
