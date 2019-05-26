84a07a933b075acf6896441ebe780059f55956d5
abhi-biswas <mpsabhijeet@gmail.com>
2019-05-26 17:57:12 +0530

    Timer Added.
    Signed-off-by: abhi-biswas <mpsabhijeet@gmail.com>

--- Client/Controllers/quizStudent.java
+++ Client/Controllers/quizStudent.java
@@ -3,6 +3,9 @@ package Controllers;
 import GUI.Main;
 import Request.QuestionFetchRequest;
 import ServerClasses.Question;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,22 +14,25 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
-import java.util.Timer;
+import java.util.Collections;
+
 
 public class quizStudent {
 
     private ArrayList<Question> questions;
     private ArrayList<questionDisplayContainer> questionDisplayContainers;
     private Button submitButton;
-    Timer timer;
+    int secNow ;
 
     @FXML
-    Label quizNameLabel,subjectCodeLabel,maxMarksLabel;
+    Label quizNameLabel,subjectCodeLabel,maxMarksLabel,hrLabel,minLabel,secLabel;
 
     @FXML
     VBox questionContiner;
+
 
     public quizStudent() {
         try {
@@ -34,7 +40,9 @@ public class quizStudent {
             Main.oos.flush();
             questions = (ArrayList<Question>) Main.ois.readObject();
             questionDisplayContainers = new ArrayList<>();
-            timer = new Timer();
+            secNow = Main.studentQuiz.getDuration()*60;
+
+
 
         }
         catch (Exception e)
@@ -51,10 +59,38 @@ public class quizStudent {
         quizNameLabel.setText(Main.studentQuiz.getQuizName());
         maxMarksLabel.setText(""+Main.studentQuiz.getMaxMarks());
         subjectCodeLabel.setText(""+Main.studentquizSubID);
+        Collections.shuffle(questions);
         for(Question u : questions)
         {
             questionDisplayContainers.add(new questionDisplayContainer(questionContiner,u));
         }
+
+        Timeline time = new Timeline(
+                new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        setTimerDisplay();
+                        secNow--;
+                        System.out.println(secNow);
+                        if(secNow==0)
+                        {
+                            setTimerDisplay();
+                            endQuiz();
+                        }
+                    }
+                })
+        );
+        time.setCycleCount(secNow);
+        time.playFromStart();
+
+
+
+
+
+
+
+
+
         submitButton = new Button("Submit");
         submitButton.setLayoutX(350);
         submitButton.setPrefHeight(55);
@@ -68,7 +104,6 @@ public class quizStudent {
                 alert.showAndWait();
                 if(alert.getResult()==ButtonType.NO)
                     return;
-                timer.cancel();
                 endQuiz();
             }
         });
@@ -78,8 +113,38 @@ public class quizStudent {
     public void endQuiz()
     {
         System.out.println("End Quiz Called");
+        
     }
 
+    public void setTimerDisplay()
+    {
+
+
+        int hours = secNow / 3600;
+        int secondsLeft = secNow - hours * 3600;
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String hr = "",min="",sec = "";
+        if (hours < 10)
+            hr += "0";
+        hr += hours + ":";
+
+        if (minutes < 10)
+            min += "0";
+        min += minutes + ":";
+
+        if (seconds < 10)
+            sec += "0";
+        sec += seconds ;
+
+
+        secLabel.setText(sec);
+        minLabel.setText(min);
+        hrLabel.setText(hr);
+
+
+    }
 
 
 }
